3bc7e3ae24116db72e002c622a77a164039a7612
Ênnya Gomes <ennyagomesoc@gmail.com>
2023-12-27 20:45:45 -0300

    consertado

--- lampada.js
+++ lampada.js
@@ -1,7 +1,10 @@
 function Ligar(){
-    
+    document.getElementById("lampada").src="/imagens/ligada.jpg";
 }
 
 function Desligar(){
-
+    document.getElementById("lampada").src="/imagens/desligada.jpg";
+}
+function Quebrar(){
+    document.getElementById("lampada").src="/imagens/quebrada.jpg";
 }
